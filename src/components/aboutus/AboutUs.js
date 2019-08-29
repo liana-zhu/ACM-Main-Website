e3ddf293d4386d662776910ac47be0c9eef5e075
epascua <emily.r.pascua@jpl.nasa.gov>
2019-08-29 13:33:20 -0700

    ACMW-0007: HTML is now rendered from JSON repsonse

--- src/components/aboutus/AboutUs.js
+++ src/components/aboutus/AboutUs.js
@@ -1,16 +1,4 @@
 import React from 'react'
-import Image0 from './leaders/alexis.jpg'
-
-import Image1 from './leaders/andrew.jpg'
-import Image2 from './leaders/brian.jpg'
-import Image3 from './leaders/david.jpg'
-import Image4 from './leaders/deric.jpg'
-import Image5 from './leaders/diego.jpg'
-import Image6 from './leaders/emily.jpg'
-import Image7 from './leaders/gio.jpg'
-import Image8 from './leaders/giovanni.jpg'
-import Image9 from './leaders/ingelbert.jpg'
-import Image10 from './leaders/kalvin.jpg'
 import "./aboutus.css"
 import Header from '../header/Header.js';
 
@@ -20,50 +8,6 @@ const aboutus = () => {
 
 		<div className="test">
 			<Header/>
-			<div class="container">
-			  <div class="row">
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image0}></img>
-			    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-			    </div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image2}></img>
-			    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers"src={Image1}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			  </div>
-			  <div class="row">
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image4}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image5}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers"src={Image6}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			  </div>
-			  <div class="row">
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image7}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers" src={Image8}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			    <div class="col-sm">
-			      <img className="image-cropper profile-pic officers"src={Image9}></img>
-			    				    	<span className="highlight-text-style2"><b><i>  FirstName LastName</i></b></span> | ACM Leader Title<br></br>
-</div>
-			  </div>
-			</div>
 		</div>
 	)
 }
--- src/components/news/newshome/NewsHomeBox.js
+++ src/components/news/newshome/NewsHomeBox.js
@@ -10,7 +10,7 @@ const NewsHomeBox =(props)=> {
   	<div className="news-text">
     <div className="news-email">By {props.news.creator.email}</div>
     <div><b>{props.news.summary}</b></div>
-    <div><div className="news-description">{props.news.description}</div>
+    <div><div dangerouslySetInnerHTML={{ __html: props.news.description }} className="news-description"></div>
     <div id="date">{props.news.start.dateTime}</div>
     <div id="time">{props.news.start.dateTime}</div>
     </div>
