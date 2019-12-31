be60b304ca4a302e2822b33c667d92e82449f6f2
dtang9 <tangd954@gmail.com>
2019-12-30 16:42:08 -0800

    Issue: Added multi carousel

--- src/components/professional/Professional.js
+++ src/components/professional/Professional.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './professional.css';
 import CurrentEventsModal from './CurrentEventsModal';
 import CardImageModal from './CardImageModal';
-import { Card, Button, Jumbotron, Container, Row, Col, Carousel, Image } from 'react-bootstrap';
+import { Card, Button, Jumbotron, Container, Row, Col, Image } from 'react-bootstrap';
+
+/*
+To install, go to project directory and run this on command line:
+$ npm install react-multi-carousel --save
+*/
+//Imports for multi carousel
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 // This component won't hold a state for not but decided
 // to make it into a Class component for now
@@ -17,6 +25,24 @@ class Professional extends React.Component {
     }
 
     render() {
+        //const responsive is used for multi carousel
+        const responsive = {
+            desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 4,
+                slidesToSlide: 3, // optional, default to 1.
+            },
+            tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 2,
+                slidesToSlide: 2, // optional, default to 1.
+            },
+            mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1,
+                slidesToSlide: 1, // optional, default to 1.
+            },
+        };
         return (
             <div className="events-container">
                 {/* <div><br/></div>
@@ -47,10 +73,10 @@ class Professional extends React.Component {
                     </Row>
 
                     <Row className="current-events-images">
-                        <Col><Image src={require('./images/internship.png')} fluid /></Col>
-                        <Col><Image src={require('./images/resume_workshop.png')} fluid /></Col>
-                        <Col><Image src={require('./images/pizza_and_boba.png')} fluid /></Col>
-                        <Col><Image src={require('./images/joker.png')} fluid /></Col>
+                        <Col sm><Image src={require('./images/internship.png')} thumbnail /></Col>
+                        <Col sm><Image src={require('./images/resume_workshop.png')} thumbnail /></Col>
+                        <Col sm><Image src={require('./images/pizza_and_boba.png')} thumbnail /></Col>
+                        <Col sm><Image src={require('./images/joker.png')} thumbnail /></Col>
                     </Row>
                 </Container>
                 <div className="gray-line"></div>
@@ -59,46 +85,32 @@ class Professional extends React.Component {
                         <p className="past-events-header">Past Events</p>
                     </Row>
                     <Row className="justify-content-md-center">
-                    <div className="past-container">
-                        <Carousel className="past-events-carousel">
-                            <Carousel.Item className="past-events-carousel">
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem', marginRight: '1rem' }} src={require('./images/awards_ceremony.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
+                        <div className="past-container">
+                            <Carousel
+                                swipeable={false}
+                                draggable={false}
+                                showDots={true}
+                                responsive={responsive}
+                                ssr={true} // means to render carousel on server-side.
+                                infinite={true}
+                                autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                                autoPlaySpeed={5000}
+                                keyBoardControl={true}
+                                customTransition="all .5"
+                                transitionDuration={500}
+                                containerClass="carousel-container"
+                                deviceType={this.props.deviceType}
+                                dotListClass="custom-dot-list-style"
+                                itemClass="carousel-item-padding-40-px">
+                                <div><Image style={{ height: '30rem', marginRight: '1rem' }} src={require('./images/awards_ceremony.png')} thumbnail /></div>
+                                <div><Image style={{ height: '30rem' }} src={require('./images/upperclassmen_workshop.png')} thumbnail /></div>
+                                <div><Image style={{ height: '30rem' }} src={require('./images/Hackathon.png')} thumbnail /></div>
+                                <div><Image style={{ height: '30rem' }} src={require('./images/venom.png')} thumbnail /></div>
+                                <div><Image style={{ height: '30rem' }} src={require('./images/intro_web_workshop.png')} thumbnail /></div>
+                                <div><Image style={{ height: '30rem' }} src={require('./images/joker.png')} thumbnail /></div>
+                            </Carousel>;
 
-                            <Carousel.Item>
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem' }} src={require('./images/upperclassmen_workshop.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem' }} src={require('./images/Hackathon.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem' }} src={require('./images/venom.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem' }} src={require('./images/intro_web_workshop.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
-
-                            <Carousel.Item>
-                                <div className="carousel-image">
-                                    <Image style={{ height: '30rem' }} src={require('./images/joker.png')} thumbnail />
-                                </div>
-                            </Carousel.Item>
-
-                        </Carousel>
-                    </div>
+                        </div>
                     </Row>
                 </Container>
 
