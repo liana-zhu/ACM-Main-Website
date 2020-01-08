7dda8df0e58429c03dc48220ccbb9950842a8fe5
dtang9 <tangd954@gmail.com>
2020-01-07 22:00:25 -0800

    Issue: Created About Us page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ class HeaderCarousel extends React.Component {
     render (){
         return( 
             <div className="parallax_style2">
-                	<span className="header-text"><span className="highlight-text"><b><i>#ACM</i></b></span> Voted Best Organization of the 2018-2019</span>
+                	<span className="header-text"><span className="highlight-text"><b><i>#ACM</i></b></span> Voted Best Organization of the Year for 2018-2019</span>
              </div>);
     }
 }
