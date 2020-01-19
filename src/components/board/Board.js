ccffe0114434a862bd6b1a906051201149e3f57a
dtang9 <tangd954@gmail.com>
2020-01-18 21:39:34 -0800

    ACMW-0037: Rearranged leaders on board page

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -240,13 +240,13 @@ class Board extends React.Component {
                         <div className="col-leaders">
                           <img className="image-cropper profile-pic " src={Randy}></img>
                           <div className="officer-info">
-                            <h2>Randal Moss</h2>ACM Advisor
+                            <h2>Randal Moss</h2>Advisor
                             </div>
                         </div>
                         <div className="col-leaders">
                           <img className="image-cropper profile-pic " src={Wilson2019}></img>
                           <div className="officer-info">
-                            <h2>Wilson Thomas</h2>ACM Advisor
+                            <h2>Wilson Thomas</h2>Advisor
                             </div>
                         </div>
                       </div>
@@ -284,9 +284,9 @@ class Board extends React.Component {
                             </div>
                         </div>
                         <div className="col-leaders">
-                          <img className="image-cropper profile-pic " src={GioG2018}></img>
+                          <img className="image-cropper profile-pic " src={GeovannyH2018}></img>
                           <div className="officer-info">
-                            <h2>Giovanni Garcia</h2>VP of Internal Affairs
+                            <h2>Geovanny Huerta</h2>Secretary
                             </div>
                         </div>
                       </div>
@@ -294,24 +294,24 @@ class Board extends React.Component {
                         <div className="col-leaders">
                           <img
                             className="image-cropper profile-pic "
-                            src={Vacant}
+                            src={GioG2018}
                           ></img>
                           <div className="officer-info">
-                            <h2>Vacant</h2>VP of External Affairs
+                            <h2>Giovanni Garcia</h2>VP of Internal Affairs
                             </div>
                         </div>
                         <div className="col-leaders">
-                          <img className="image-cropper profile-pic " src={Brian}></img>
+                          <img className="image-cropper profile-pic " src={Vacant}></img>
                           <div className="officer-info">
-                            <h2>Brian Hernandez</h2>Project Manager
+                            <h2>Vacant</h2>VP of External Affairs
                             </div>
                         </div>
                       </div>
                       <div className="row-leaders">
                         <div className="col-leaders">
-                          <img className="image-cropper profile-pic " src={GeovannyH2018}></img>
+                          <img className="image-cropper profile-pic " src={Brian}></img>
                           <div className="officer-info">
-                            <h2>Geovanny Huerta</h2>Secretary
+                            <h2>Brian Hernandez</h2>Project Manager
                             </div>
                         </div>
                         <div className="col-leaders">
@@ -342,9 +342,9 @@ class Board extends React.Component {
                       </div>
                       <div className="row-leaders">
                         <div className="col-leaders">
-                          <img className="image-cropper profile-pic " src={Ryan}></img>
+                          <img className="image-cropper profile-pic " src={GioC2018}></img>
                           <div className="officer-info">
-                            <h2>Ryan Dunning</h2>Web/Networking Officer
+                            <h2>Giovanni Castellanos</h2>Digital Media Officer
                             </div>
                         </div>
                         <div className="col-leaders">
@@ -356,9 +356,9 @@ class Board extends React.Component {
                       </div>
                       <div className="row-leaders">
                         <div className="col-leaders">
-                          <img className="image-cropper profile-pic " src={GioC2018}></img>
+                          <img className="image-cropper profile-pic " src={Ryan}></img>
                           <div className="officer-info">
-                            <h2>Giovanni Castellanos</h2>Digital Media Officer
+                            <h2>Ryan Dunning</h2>Web/Networking Officer
                             </div>
                         </div>
                       </div>
