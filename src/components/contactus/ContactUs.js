492a879919f92905648950d4e6a06f7e743589c0
Anthony Viramontes <amviramontes34@yahoo.com>
2021-08-11 00:50:29 -0700

    Removed contact us text underneath acm logo. Merge acm logo and acm social media blocks together. Shortened the width on the form.  Removed unnecessary code.
    Cleaned up page so it visibly looks better. looks good in mobile view.

--- src/components/contactus/ContactUs.js
+++ src/components/contactus/ContactUs.js
@@ -13,25 +13,8 @@ class ContactUs extends React.Component {
 				<div className="contact-us-card">
 					<Card className="text-center">
 						<img src={logo} className="contact-us-logo" alt="ACM logo" />
-						<div className="contact-info">
-							<p>
-								<b>Contact: </b>
-								{/* <a href="mailto:acm.calstatela@gmail.com" target="_top">acm.calstatela@gmail.com</a> */}
-								acm.calstatela@gmail.com
-						</p>
-							{/* <p>
-							<b>Office Hours:</b> Thursdays, 3-4 pm @ET C-245
-						</p> */}
-							<p>
-								There will be no office hours
-						</p>
-						</div>
-					</Card>
-				</div>
 
-				<div className="contact-us-card">
-					<Card className="text-center">
-						<div>
+						<div className="contact-us-block">
 							<h1>Social Media</h1>
 							<a href="https://www.instagram.com/calstatela_acm/?hl=en">
 								{/* <FontAwesnomeIcon className="iconBrand" size="2x" icon={faInstagram}></FontAwesomeIcon> */}
@@ -51,29 +34,28 @@ class ContactUs extends React.Component {
 
 				<div className="contact-us-card">
 					<Card className="text-center" text="white">
-						<h1>Contact Us</h1>
-						<form action="mailto:acm.calstatela@gmail.com?Subject=Contacting%20You%20From%20Website" method="POST" enctype="text/plain">
-							<div className="txtb">
-								<label for="name" className="card-bg">Full Name :</label>
-								<input type="text" id="name" name="name" className="card-bg" placeholder="Enter Your Name" />
-							</div>
-
-							<div className="txtb">
-								<label for="email" className="card-bg">Email: </label>
-								<input type="text" id="email" name="email" className="card-bg" placeholder="Enter Your Email" />
-							</div>
-
-							<div className="txtb">
-								<label for="phoneNo" className="card-bg">Phone Number :</label>
-								<input type="text" id="phoneNo" name="phoneNo" className="card-bg" placeholder="Enter Your Phone Number" />
-							</div>
-
-							<div className="txtb">
-								<label for="message" className="card-bg">Message: </label>
-								<textarea id="body" name="message" className="card-bg"></textarea>
-							</div>
-							<input type="submit" value="Send" id="btn-form" />
-						</form>
+						<div className="contact-us-block">
+							<h1>Contact Us</h1>
+							<form action="mailto:acm.calstatela@gmail.com?Subject=Contacting%20You%20From%20Website" method="POST" enctype="text/plain">
+								<div className="txtb">
+									<label for="name" className="card-bg">Full Name :</label>
+									<input type="text" id="name" name="name" className="card-bg" placeholder="Enter Your Name" />
+								</div>
+								<div className="txtb">
+									<label for="email" className="card-bg">Email: </label>
+									<input type="text" id="email" name="email" className="card-bg" placeholder="Enter Your Email" />
+								</div>
+								<div className="txtb">
+									<label for="phoneNo" className="card-bg">Phone Number :</label>
+									<input type="text" id="phoneNo" name="phoneNo" className="card-bg" placeholder="Enter Your Phone Number" />
+								</div>
+								<div className="txtb">
+									<label for="message" className="card-bg">Message: </label>
+									<textarea id="body" name="message" className="card-bg"></textarea>
+								</div>
+								<input type="submit" value="Send" id="btn-form" />
+							</form>
+						</div>
 					</Card>
 				</div>
 			</div>
