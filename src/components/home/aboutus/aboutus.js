b2229b8e346d863e5111f94bc65ac4d43d784b29
Ryanseanlee <71867199+Ryanseanlee@users.noreply.github.com>
2022-03-19 12:44:04 -0700

    home page slide in animation

--- src/components/home/aboutus/aboutus.js
+++ src/components/home/aboutus/aboutus.js
@@ -1,29 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import "animate.css/animate.min.css";
 
-import './aboutus.scss';
+import "./aboutus.scss";
 
 class AboutUs extends Component {
-    render() {
-        return (
-            <section className="AboutUs-tease">
-                <div className="left">
-                    <div className="content">
-                        <div className="subTitle"> acm@calstatela </div>
-                        <div className="title"> Who are we? </div>
-                        <div className="description"> We’re a chapter of Association for Computing Machinery at California State University, Los Angeles. We bring students with a common passion and devotion into the field of computing. </div>
-                        <div className="link-btn mt-4">
-                            <a className="label px-5" href="/aboutus">
-                                learn more
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="right">
-
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <AnimationOnScroll
+        animateIn="animate__slideInLeft"
+        delay="1"
+        animateOnce="true"
+      >
+        <section className="AboutUs-tease">
+          <div className="left">
+            <div className="content">
+              <div className="subTitle"> acm@calstatela </div>
+              <div className="title"> Who are we? </div>
+              <div className="description">
+                {" "}
+                We’re a chapter of Association for Computing Machinery at
+                California State University, Los Angeles. We bring students with
+                a common passion and devotion into the field of computing.{" "}
+              </div>
+              <div className="link-btn mt-4">
+                <a className="label px-5" href="/aboutus">
+                  learn more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="right"></div>
+        </section>
+      </AnimationOnScroll>
+    );
+  }
 }
 
 export default AboutUs;
--- src/components/home/membership/membership.js
+++ src/components/home/membership/membership.js
@@ -1,29 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import "animate.css/animate.min.css";
 
-import './membership.scss';
+import "./membership.scss";
 
 class Membership extends Component {
-    render() {
-        return(
-            <section className="Membership-tease">
-                <div className="left">
-                    <div className="content">
-                        <div className="subTitle"> membership </div>
-                        <div className="title"> Become a binary </div>
-                        <div className="description"> Become part of Cal State LA’s largest computer science community. Members receive many benefits, including professional development, career growth and networking opportunities. </div>
-                        <div className="link-btn mt-4">
-                            <a className="label px-5" href="/membership">
-                                join today
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="right">
-
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <AnimationOnScroll
+        animateIn="animate__slideInLeft"
+        delay="1"
+        animateOnce="true"
+      >
+        <section className="Membership-tease">
+          <div className="left">
+            <div className="content">
+              <div className="subTitle"> membership </div>
+              <div className="title"> Become a binary </div>
+              <div className="description">
+                {" "}
+                Become part of Cal State LA’s largest computer science
+                community. Members receive many benefits, including professional
+                development, career growth and networking opportunities.{" "}
+              </div>
+              <div className="link-btn mt-4">
+                <a className="label px-5" href="/membership">
+                  join today
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="right"></div>
+        </section>
+      </AnimationOnScroll>
+    );
+  }
 }
 
 export default Membership;
