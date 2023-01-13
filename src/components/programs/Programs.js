701ecf9d611a3384a12be16652e5e7e94809e0f7
Brian Pham <brianpt1106@gmail.com>
2023-01-12 21:31:24 -0800

    Revert "updated pro-dev page (revisions)"
    This reverts commit 050cfdaa64223e1679c1473f14acd513abec8632.

--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -60,7 +60,7 @@ class Programs extends React.Component {
 
         <div className="disclaimer-body">
           <h3 className="disclaimer_header">
-            <b>Disclaimers...</b>
+            <b>Disclaimers</b>
           </h3>
           <span className="disclaimer">
             <b>
@@ -215,13 +215,13 @@ class Programs extends React.Component {
                                     <b>Building your Resume/LinkedIn</b>
                                   </div>
                                 </div>
-                                <div className="skill-item2 col-lg-4 col-md-6 col-12">
+                                <div className="skill-item col-lg-4 col-md-6 col-12">
                                   <div className="icon-box">
                                     <PeopleFill className="icon" />
                                     <b>Mock Interviews</b>
                                   </div>
                                 </div>
-                                <div className="skill-item2 col-lg-4 col-md-6 col-12">
+                                <div className="skill-item col-lg-4 col-md-6 col-12">
                                   <div className="icon-box">
                                     <CashCoin className="icon" />
                                     <b>Financial Management</b>
@@ -252,7 +252,6 @@ class Programs extends React.Component {
                             </Tab.Content>
                             <Button
                               variant="success"
-                              className = "join-button"
                               size="lg"
                               href="https://docs.google.com/forms/d/e/1FAIpQLSf6apdzV5q2jrsJZCbsfV-SGx2cZimQyU3D60N3AUvHjhW40g/viewform"
                             >
@@ -359,13 +358,13 @@ class Programs extends React.Component {
                             <Tab.Content className="programs-tab-content poster">
                               <Tab.Pane eventKey="menteeRole"></Tab.Pane>
                             </Tab.Content>
-                            {/*<Button
+                            <Button
                               variant="success"
                               size="lg"
                               href="https://docs.google.com/forms/d/e/1FAIpQLSf6apdzV5q2jrsJZCbsfV-SGx2cZimQyU3D60N3AUvHjhW40g/viewform"
                             >
                               Join Now!
-                          </Button>*/}
+                            </Button>
                             <br />
                           </div>
                         </Tab.Pane>
