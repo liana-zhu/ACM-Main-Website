25cbfcb77fd237a0a430c4e480da1b42ff707758
LudiJolo <joloh18@yahoo.com>
2023-02-02 21:12:57 -0800

    banner slideshow in calendar page
    - implemented slideshow component to the calendar page
    - added transparent background to the text
    - added banner with more flyers

---
+++ src/components/events/SlideShow.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './slideshow.scss';
+
+class Slideshow extends Component {
+    render() {
+        return (
+            // Code for slideshow: https://css-tricks.com/infinite-all-css-scrolling-slideshow/
+            <div class="tech-slideshow">
+                <div class="mover-1"></div>
+            </div>
+        )
+    }
+}
+
+export default Slideshow;
