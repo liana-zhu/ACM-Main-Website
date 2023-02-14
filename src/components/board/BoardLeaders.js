1334c8830efc87de1f513311d98b4acdf4bb1587
Brian Pham <brianpt1106@gmail.com>
2023-02-14 15:49:45 -0800

    updates to home page

--- src/components/board/BoardLeaders.js
+++ src/components/board/BoardLeaders.js
@@ -12,10 +12,13 @@ class BoardLeaders extends React.Component {
                 </div>
                 {/*<div class = "buttons">
                     <div class = "linkin">
-                        <a href= {this.props.linkedin}><input type="image" src="https://pecservice.nl/assets/uploads/2016/02/linkedin-icon.png" name="submit" width="35" height="35" alt="submit"/></a>
+                        <a href= {this.props.linkedin}><input type="image" src="https://pecservice.nl/assets/uploads/2016/02/linkedin-icon.png" name="submit" width="35" height="35" alt="submit" id="link"/></a>
                     </div>
                     <div class = "email">
-                        <a href= {this.props.email}><input type="image" src="https://www.iconpacks.net/icons/2/free-send-mail-icon-2574-thumb.png" name="submit" width="45" height="45" alt="submit"/></a>
+                        <a href= {this.props.email}><input type="image" src="https://www.iconpacks.net/icons/2/free-send-mail-icon-2574-thumb.png" name="submit" width="45" height="45" alt="submit" id="email"/></a>
+                    </div>
+                    <div class = "git">
+                        <a href= {this.props.github}><input type="image" src="https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png" name="submit" width="35" height="35" alt="submit" id="git"/></a>
                     </div>
                 </div>*/}
             </div>
